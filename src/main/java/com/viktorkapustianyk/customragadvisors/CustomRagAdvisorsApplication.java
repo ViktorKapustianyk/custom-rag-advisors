69d85def866daf914ad176cf373970063d19e949
Viktor Kapustianyk <vityakap@gmail.com>
2026-08-10 14:28:31 +0300

    Initial commit

---
+++ src/main/java/com/viktorkapustianyk/customragadvisors/CustomRagAdvisorsApplication.java
@@ -0,0 +1,13 @@
+package com.viktorkapustianyk.customragadvisors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRagAdvisorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomRagAdvisorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viktorkapustianyk/customragadvisors/CustomRagAdvisorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viktorkapustianyk.customragadvisors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRagAdvisorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
