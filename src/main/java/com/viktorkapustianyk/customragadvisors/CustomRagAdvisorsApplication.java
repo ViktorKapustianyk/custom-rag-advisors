71792a1cf1227f3ac785ec8df7eff61fdd3ac932
Viktor Kapustianyk <vityakap@gmail.com>
2026-08-10 16:32:49 +0300

    Add ChatClient integration, database (PostgreSQL) setup, and Docker Compose configuration

--- src/main/java/com/viktorkapustianyk/customragadvisors/CustomRagAdvisorsApplication.java
+++ src/main/java/com/viktorkapustianyk/customragadvisors/CustomRagAdvisorsApplication.java
@@ -1,13 +1,25 @@
 package com.viktorkapustianyk.customragadvisors;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomRagAdvisorsApplication {
 
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(CustomRagAdvisorsApplication.class, args);
+
+        ChatClient chatClient = SpringApplication.run(CustomRagAdvisorsApplication.class, args).getBean(ChatClient.class);
+        System.out.println(chatClient.prompt()
+                .user("Give me the text of the song Bohemian Rhapsody")
+                .call()
+                .content());
     }
 
 }
